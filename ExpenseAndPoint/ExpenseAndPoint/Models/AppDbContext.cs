c9f9ad9f2cb197d31ccf75f4f945973f8bf58fc7
de4rbe4r <artemdeyev@gmail.com>
2023-09-04 17:58:55 +0400

    Create project with ASP.NET and ReactsJS, create diagram

---
+++ ExpenseAndPoint/ExpenseAndPoint/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseAndPoint.Models
+{
+    public class AppDbContext : DbContext 
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<Category> Categories { get; set; } 
+    }
+}
---
+++ ExpenseAndPoint/ExpenseAndPoint/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace ExpenseAndPoint.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public ICollection<Expense>? Expenses { get; set; }
+    }
+}
