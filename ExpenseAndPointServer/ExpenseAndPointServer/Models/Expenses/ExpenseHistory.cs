f5a3228915fbe5fbc504f99087ac78e0888c6165
de4rbe4r <artemdeyev@gmail.com>
2023-10-31 20:28:59 +0400

    Начало создания модели сохранения истории расходов

---
+++ ExpenseAndPointServer/ExpenseAndPointServer/Models/Expenses/ExpenseHistory.cs
@@ -0,0 +1,53 @@
+﻿using ExpenseAndPointServer.Models.Categories;
+using ExpenseAndPointServer.Models.Users;
+
+namespace ExpenseAndPointServer.Models.Expenses
+{
+    public class ExpenseHistory
+    {
+        /// <summary>
+        /// Дата создания записи
+        /// </summary>
+        public DateTime DateCreated { get; set; }
+
+        /// <summary>
+        /// Имя пользователя
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Действие
+        /// </summary>
+        public string Action { get; set; }
+
+        /// <summary>
+        /// Сумма расхода
+        /// </summary>
+        public float Amount { get; set; }
+
+        /// <summary>
+        /// Дата и время
+        /// </summary>
+        public DateTime DateTime { get; set; }
+
+        /// <summary>
+        /// Идентификатор категории
+        /// </summary>
+        public int CategoryId { get; set; }
+
+        /// <summary>
+        /// Идентификатор пользователя
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// Категория
+        /// </summary>
+        public Category Category { get; set; }
+
+        /// <summary>
+        /// Пользователь
+        /// </summary>
+        public User User { get; set; }
+    }
+}
